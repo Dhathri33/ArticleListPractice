4d646b3aefe1c91326547cadcf99c57d5af394a8
Dhathri33 <dbathini@luc.edu>
2025-10-13 12:30:11 -0500

    Initial Commit

---
+++ ArticleListPractice/ArticleListPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleListPracticeApp.swift
+//  ArticleListPractice
+//
+//  Created by Dhathri Bathini on 10/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ArticleListPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
